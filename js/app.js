30ccd20adf30395dfe69d124eb6198a2725ac9d2
alex <polovinkin.a.c@mail.ru>
2021-08-01 13:39:15 +0300

    result

--- js/app.js
+++ js/app.js
@@ -30,21 +30,101 @@ return {
  function listenerSubmit(evt) {
     evt.preventDefault();
 
-    const amountInputEl = document.getElementById('amount-input');
-    const amountInput = Number(amountInputEl.value);
+
+     const amountInputErrorEl = document.getElementById('amount-error');
+     const periodInputErrorEl = document.getElementById('period-error');
+     const totalEl = document.getElementById('total');
+     const profitEl =document.getElementById('profit');
+     const percentEl = document.getElementById('percent');
+
+     amountInputErrorEl.textContent = '';
+     periodInputErrorEl.textContent = '';
+     totalEl.textContent = '';
+     profitEl.textContent = '';
+     percentEl.textContent = '';
+
+     const amountInputEl = document.getElementById('amount-input');
+     const amountInput = Number(amountInputEl.value);
+    if (Number.isNaN(amountInput)) {
+        amountInputErrorEl.textContent = 'Неверное значение. Введите число, например: 15000';
+         return;
+    }
+    if (amountInput < 15000) {
+        amountInputErrorEl.textContent = 'Неверное значение. Минимальная сумма: 15000 ₽';
+        return;
+    }
+    if (amountInput > 50000000) {
+        amountInputErrorEl.textContent = 'Неверное значение. Максимальная сумма: 50000000 ₽';
+        return;
+    }
 
     const periodInputEl =document.getElementById('period-input');
     const periodInput = Number(periodInputEl.value);
-
+    if (Number.isNaN(periodInput)){
+        periodInputErrorEl.textContent = 'Неверное значение. Введите число месяцев, например: 3';
+        return;
+    }
+    if (periodInput < 3){
+        periodInputErrorEl.textContent = 'Неверное значение. Минимальный период: 3 месяца';
+        return;
+    }
+    if (periodInput > 18){
+        periodInputErrorEl.textContent = 'Неверное значение. Максимальный период: 18 месяцев';
+        return;
+    }
 
     const result = calculateInterest(amountInput, periodInput);
-    const totalEl = document.getElementById('total');
-    totalEl.textContent =`${result.totalInput}`;
-    const profitEl =document.getElementById('profit');
-    profitEl.textContent = `${result.profitInput}`;
-    const percentEl = document.getElementById('percent');
-    percentEl.textContent = `${result.percent}`;
 
+    totalEl.textContent =`${result.totalInput}`;
+    if (Number.isNaN(amountInput)){
+        totalEl.textContent = '';
+        return;
+    }
+    if (amountInput < 15000){
+        totalEl.textContent= '';
+        return;
+    }
+    if (amountInput > 50000000){
+        totalEl.textContent = '';
+        return;
+    }
+
+    profitEl.textContent = `${result.profitInput}`;
+    if (Number.isNaN(periodInput)){
+        profitEl.textContent = '';
+        return;
+    }
+    if (periodInput < 3){
+        profitEl.textContent ='';
+        return;
+    }
+    if ((periodInput > 18)){
+        profitEl.textContent = '';
+        return;
+    }
+
+    percentEl.textContent = `${result.percent}`;
+    if (Number.isNaN((periodInput && amountInput))){
+        percentEl.textContent = '';
+        return;
+    }
+    if (amountInput< 15000 && periodInput < 3){
+        percentEl.textContent = '';
+        return;
+    }
+    if (amountInput > 50000000 && periodInput > 18){
+        percentEl.textContent = '';
+        return;
+    }
 }
 const formEl = document.getElementById('deposit-form');
 formEl.addEventListener('submit', listenerSubmit);
+
+
+
+
+
+
+
+
+
